a4823debf4a0239344b390c15a63103f4e33546e
dacid44 <dacid44@gmail.com>
2022-01-17 17:39:03 -0600

    fixed unix build script

--- build.rs
+++ build.rs
@@ -1,10 +1,13 @@
 #[cfg(windows)]
 extern crate winres;
 
+#[cfg(windows)]
 fn main() {
-    if cfg!(target_os = "windows") {
-        let mut res = winres::WindowsResource::new();
-        res.set_icon("src/icon/android_apps_egui.ico");
-        res.compile().unwrap();
-    }
+    let mut res = winres::WindowsResource::new();
+    res.set_icon("src/icon/android_apps_egui.ico");
+    res.compile().unwrap();
+}
+
+#[cfg(unix)]
+fn main() {
 }
